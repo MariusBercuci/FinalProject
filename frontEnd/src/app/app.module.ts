9278cce8ce315e33b63bd6f462b1e4d8b145be1b
Marius <marian.marius.bercuci@gmail.com>
2022-10-15 00:05:20 +0300

    cart

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -6,12 +6,17 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
 import { SearchComponent } from './components/partial/search/search.component';
 import { TagsComponent } from './components/partial/tags/tags.component';
+import { ProductPageComponent } from './components/pages/product-page/product-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partial/title/title.component';
 
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'search/:searchTerm', component:HomeComponent},
-  {path: 'tag/:tag', component:HomeComponent}
+  {path: 'tag/:tag', component:HomeComponent},
+  {path:'product/:id', component:ProductPageComponent},
+  {path:'cart-page', component: CartPageComponent}
 ];
 
 @NgModule({
@@ -21,6 +26,9 @@ const routes: Routes = [
     HomeComponent,
     SearchComponent,
     TagsComponent,
+    ProductPageComponent,
+    CartPageComponent,
+    TitleComponent,
 
 
   ],
